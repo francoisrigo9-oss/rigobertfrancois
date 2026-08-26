ead667b63b9df1d9223cd85a4cfdacbd034d21b4
francoisrigo9-oss <306826517+francoisrigo9-oss@users.noreply.github.com>
2026-08-26 15:10:08 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,25 @@
-self.addEventListener('install', e => {
+const CACHE_NAME = "rigobert-app-v1";
+const FILES_TO_CACHE = [
+  "/rigobertfrancois/",
+  "/rigobertfrancois/index.html",
+  "/rigobertfrancois/projets.html",
+  "/rigobertfrancois/contact.html",
+  "/rigobertfrancois/apropos.html",
+  "/rigobertfrancois/actualite.html",
+  "/rigobertfrancois/francois.jpg.jpeg",
+  "/rigobertfrancois/manifest.json"
+];
+
+// Installer et mettre en cache
+self.addEventListener("install", (e) => {
   e.waitUntil(
-    caches.open('rigodev-v1').then(cache => {
-      return cache.addAll(['/', '/index.html', '/manifest.json']);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
   );
 });
-self.addEventListener('fetch', e => {
+
+// Servir depuis le cache si pas internet
+self.addEventListener("fetch", (e) => {
   e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
+    caches.match(e.request).then((response) => response || fetch(e.request))
   );
 });
